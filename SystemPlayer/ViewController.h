b5222a617f31987890ec60592a6e5ec05c513c5e
codeliu6572 <codeliu6572@163.com>
2016-06-02 09:35:33 +0800

    Initial Commit

---
+++ SystemPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SystemPlayer
+//
+//  Created by 刘浩浩 on 16/6/2.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
